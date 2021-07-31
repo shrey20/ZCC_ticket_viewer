f93583e019101fb8501a5a429992cbbe09036dfb
shrey20 <53436076+shrey20@users.noreply.github.com>
2021-07-31 18:40:21 +0530

    added list_reformat()

--- ticket_viewer.py
+++ ticket_viewer.py
@@ -13,27 +13,45 @@ class ticket_viewer:
         self.pwd = pwd
 
     def list_api_call(self, url= 'https://zccstudentshelp.zendesk.com/api/v2/tickets.json', param = {'page[size]':'25'}):
-        while url:
-            try:
-                response = requests.get(url, auth=(self.user, self.pwd), params=param)
-                response.raise_for_status()
-                return response
-            except HTTPError as http_err:
-                if response.status_code == 400:
-                    print('{0:s}: Bad request, make sure you typed everything correctly'.format(http_err))
-                elif response.status_code == 403:
-                    print('{0:s}: The user does not have permission for api call'.format(http_err))
-                elif response.status_code == 400:
-                    print('{0:s}: Check if the url entered is correct')
-                else:
-                    print(f'HTTP error occurred: {http_err}')
-            except Exception as err:
-                    print(f'Other error occurred: {err}')
+        try:
+            response = requests.get(url, auth=(self.user, self.pwd), params=param)
+            response.raise_for_status()
+            return response
+        except HTTPError as http_err:
+            if response.status_code == 400:
+                print(f'Bad request, make sure you typed everything correctly. {http_err}')
+            elif response.status_code == 403:
+                print(f'The user does not have permission for api call. {http_err}')
+            elif response.status_code == 404:
+                print(f'Check if the url entered is correct. {http_err}')
+            else:
+                print(f'HTTP error occurred: {http_err}')
+        except Exception as err:
+                print(f'Other error occurred: {err}')
+
+    def list_reformat(self, response):
+
+        def wrap(row):
+            return '\n'.join(re.findall('.{1,60}', row))
+
+        if response:
+            df =pd.DataFrame(response['tickets'])
+            if not df.empty:
+                df.fillna("Unknown", inplace=True)
+                data = df.loc[:, ['id', 'created_at', 'subject', 'priority', 'status', 'description']]
+                desc_wraped = data.description.apply(wrap)
+                del data['description']
+                data['description'] = desc_wraped
+                return data
+        else:
+            print("No tickets available to show!")
+
 
 def main():
     obj = ticket_viewer()
     x = obj.list_api_call()
-    print (x.json())
+    data = obj.list_reformat(x.json())
+    print(data)
 
 if __name__ == "__main__":
     main()
