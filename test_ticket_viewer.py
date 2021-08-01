b7c8f928d06e34173df5604d7ba3bd3493c64cc6
shrey20 <53436076+shrey20@users.noreply.github.com>
2021-08-01 12:37:18 +0530

    Added comments

--- test_ticket_viewer.py
+++ test_ticket_viewer.py
@@ -3,6 +3,12 @@ from ticket_viewer import ticket_viewer
 
 
 def test_list_api_call(capsys):
+    """
+    Checks if the list_api_call works for given credentials and url
+    :param capsys: Captures stdout and stderr
+    """
+
+    # obj1 and obj2 have valid credential while obj3 has invalid credentials
     obj1 = ticket_viewer()
     obj2 = ticket_viewer(pwd="e081nL1b2wzlokyTQMcsHHutYMKScLkyEqc1KbUz")
     obj3 = ticket_viewer(pwd= "abcd")
@@ -10,21 +16,29 @@ def test_list_api_call(capsys):
 
     response = obj1.list_api_call()
     assert response.status_code == 200
+    assert response.url == "https://zccstudentshelp.zendesk.com/api/v2/tickets.json?page%5Bsize%5D=25"
 
     response = obj2.list_api_call()
     assert response.status_code == 200
+    assert response.url == "https://zccstudentshelp.zendesk.com/api/v2/tickets.json?page%5Bsize%5D=25"
 
+    #Accessing the api with invalid credential
     obj3.list_api_call()
     captured = capsys.readouterr()
-    # print(captured.out[:123])
     assert captured.out[:59] == "HTTP error occurred: 401 Client Error: Unauthorized for url"
 
+    #Passing in the wrong url
     obj1.list_api_call(url="https://zccstudentshelp.zendesk.com/api/v2/tic")
     captured = capsys.readouterr()
     assert captured.out[:72] == "Check if the url entered is correct. 404 Client Error: Not Found for url"
 
 
 def test_list_reformat(capsys):
+    """
+    Checks if the method returns the correct dataframe for a given response
+    (Or output message for no response)
+    :param capsys: Captures stdout and stderr
+    """
     obj = ticket_viewer()
 
     response = obj.list_api_call()
@@ -43,6 +57,11 @@ def test_list_reformat(capsys):
 
 
 def test_single_api_call(capsys):
+    """
+    Checks if single_api_call returns correct response for a given id
+    :param capsys: Captures stdout and stderr
+    :return:
+    """
     obj1 = ticket_viewer()
     obj2 = ticket_viewer(pwd="abcd")
     exist = False
