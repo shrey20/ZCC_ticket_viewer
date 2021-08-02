5694e46521c02e35b62641b25325ba25d255ac67
shrey20 <53436076+shrey20@users.noreply.github.com>
2021-08-02 15:40:02 +0530

    Update test_ticket_viewer.py

--- test_ticket_viewer.py
+++ test_ticket_viewer.py
@@ -1,5 +1,6 @@
 import pytest
 from ticket_viewer import ticket_viewer
+from ticket_viewer import main
 
 
 def test_list_api_call(capsys):
@@ -15,22 +16,24 @@ def test_list_api_call(capsys):
 
 
     response = obj1.list_api_call()
-    assert response.status_code == 200
-    assert response.url == "https://zccstudentshelp.zendesk.com/api/v2/tickets.json?page%5Bsize%5D=25"
+    if response:
+        assert response.status_code == 200
+        assert response.url == "https://zccstudentshelp.zendesk.com/api/v2/tickets.json?page%5Bsize%5D=25"
 
     response = obj2.list_api_call()
-    assert response.status_code == 200
-    assert response.url == "https://zccstudentshelp.zendesk.com/api/v2/tickets.json?page%5Bsize%5D=25"
+    if response:
+        assert response.status_code == 200
+        assert response.url == "https://zccstudentshelp.zendesk.com/api/v2/tickets.json?page%5Bsize%5D=25"
 
     #Accessing the api with invalid credential
     obj3.list_api_call()
     captured = capsys.readouterr()
-    assert captured.out[:59] == "HTTP error occurred: 401 Client Error: Unauthorized for url"
+    assert "HTTP error occurred: 401 Client Error:" in captured.out
 
     #Passing in the wrong url
     obj1.list_api_call(url="https://zccstudentshelp.zendesk.com/api/v2/tic")
     captured = capsys.readouterr()
-    assert captured.out[:72] == "Check if the url entered is correct. 404 Client Error: Not Found for url"
+    assert "Check if the url entered is correct. 404 Client Error:" in captured.out
 
 
 def test_list_reformat(capsys):
@@ -44,11 +47,14 @@ def test_list_reformat(capsys):
     response = obj.list_api_call()
     if response:
         data_frame = obj.list_reformat(response.json())
+
         if not data_frame.empty:
             if set(['id', 'created_at', 'subject', 'priority', 'status', 'description']).issubset(data_frame.columns):
                 assert True
+
             else:
                 assert False
+
 
         response = None
         obj.list_reformat(response)
@@ -58,25 +64,46 @@ def test_list_reformat(capsys):
 
 def test_single_api_call(capsys):
     """
-    Checks if single_api_call returns correct response for a given id
+    Checks if single_api_call returns correct response for a given id and credentials
     :param capsys: Captures stdout and stderr
     """
+
+    #obj1 has valid credentials while obj2 has invalid credentials
     obj1 = ticket_viewer()
     obj2 = ticket_viewer(pwd="abcd")
-    exist = False
+    exist = False # Flag to check whether id exists in the database
 
+    #Check response for various ticket ids
     response = obj1.single_api_call(1)
     if response:
         assert response.json()['ticket']['id'] == 1
+        assert response.url == "https://zccstudentshelp.zendesk.com/api/v2/tickets/1.json"
+
         exist = True
+
+    else:
+        captured = capsys.readouterr()
+        assert captured.out == "Please enter a valid ticket id.\n"
 
     response = obj1.single_api_call(10)
     if response:
         assert response.json()['ticket']['id'] == 10
+        assert response.url == "https://zccstudentshelp.zendesk.com/api/v2/tickets/10.json"
+
+    else:
+        captured = capsys.readouterr()
+        assert captured.out == "Please enter a valid ticket id.\n"
+
 
     response = obj1.single_api_call(50)
     if response:
         assert response.json()['ticket']['id'] == 50
+        assert response.url == "https://zccstudentshelp.zendesk.com/api/v2/tickets/50.json"
+
+    else:
+        captured = capsys.readouterr()
+        assert captured.out == "Please enter a valid ticket id.\n"
+
 
     obj1.single_api_call(1087)
     captured = capsys.readouterr()
@@ -85,6 +112,10 @@ def test_single_api_call(capsys):
     if exist:
         obj2.single_api_call(1)
         captured = capsys.readouterr()
-        assert captured.out[:59] == "HTTP error occurred: 401 Client Error: Unauthorized for url"
+        assert "HTTP error occurred: 401 Client Error:" in captured.out
+
+
+
+
 
 
