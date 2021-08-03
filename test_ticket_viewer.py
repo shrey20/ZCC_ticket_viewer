63813325852e76b5cc5a6cc9ea4559826db5c88c
shrey20 <53436076+shrey20@users.noreply.github.com>
2021-08-03 23:25:10 +0530

    mocked the get requests

--- test_ticket_viewer.py
+++ test_ticket_viewer.py
@@ -1,7 +1,9 @@
+import json
 import os
 import pytest
+import unittest
+from unittest.mock import patch
 from ticket_viewer import ticket_viewer
-from ticket_viewer import main
 
 
 def test_list_api_call(capsys):
@@ -10,31 +12,35 @@ def test_list_api_call(capsys):
     :param capsys: Captures stdout and stderr
     """
 
-    # obj1 and obj2 have valid credential while obj3 has invalid credentials
     obj1 = ticket_viewer()
     obj2 = ticket_viewer(pwd=os.environ.get('zcc_pwd2'))
-    obj3 = ticket_viewer(pwd= "abcd")
+    obj3 = ticket_viewer(pwd="abcd")
 
+    # mocks get request for ticket_viewer module and verifies if the right get request is made
 
-    response = obj1.list_api_call()
-    if response:
-        assert response.status_code == 200
-        assert response.url == "https://zccstudentshelp.zendesk.com/api/v2/tickets.json?page%5Bsize%5D=25"
+    with patch('ticket_viewer.requests.get') as mocked_get:
+        obj1.list_api_call()
+        mocked_get.assert_called_with('https://zccstudentshelp.zendesk.com/api/v2/tickets.json',
+                                      auth=(os.environ.get('zcc_user') + '/token', os.environ.get('zcc_pwd1')),
+                                      params={'page[size]': '25'})
 
-    response = obj2.list_api_call()
-    if response:
-        assert response.status_code == 200
-        assert response.url == "https://zccstudentshelp.zendesk.com/api/v2/tickets.json?page%5Bsize%5D=25"
+    with patch('ticket_viewer.requests.get') as mocked_get:
+        obj2.list_api_call()
+        mocked_get.assert_called_with('https://zccstudentshelp.zendesk.com/api/v2/tickets.json',
+                                      auth=(os.environ.get('zcc_user') + '/token', os.environ.get('zcc_pwd2')),
+                                      params={'page[size]': '25'})
 
-    #Accessing the api with invalid credential
-    obj3.list_api_call()
-    captured = capsys.readouterr()
-    assert "HTTP error occurred: 401 Client Error:" in captured.out
+    with patch('ticket_viewer.requests.get') as mocked_get:
+        obj3.list_api_call()
+        mocked_get.assert_called_with('https://zccstudentshelp.zendesk.com/api/v2/tickets.json',
+                                      auth=(os.environ.get('zcc_user') + '/token', "abcd"),
+                                      params={'page[size]': '25'})
 
-    #Passing in the wrong url
-    obj1.list_api_call(url="https://zccstudentshelp.zendesk.com/api/v2/tic")
-    captured = capsys.readouterr()
-    assert "Check if the url entered is correct. 404 Client Error:" in captured.out
+    with patch('ticket_viewer.requests.get') as mocked_get:
+        obj1.list_api_call(url="https://zccstudentshelp.zendesk.com/api/v2/tic")
+        mocked_get.assert_called_with("https://zccstudentshelp.zendesk.com/api/v2/tic",
+                                      auth=(os.environ.get('zcc_user') + '/token', os.environ.get('zcc_pwd1')),
+                                      params={'page[size]': '25'})
 
 
 def test_list_reformat(capsys):
@@ -43,24 +49,29 @@ def test_list_reformat(capsys):
     (Or output message for no response)
     :param capsys: Captures stdout and stderr
     """
+
     obj = ticket_viewer()
 
-    response = obj.list_api_call()
-    if response:
-        data_frame = obj.list_reformat(response.json())
+    # retrives data which contains information corresponding to a page of tickets
+    with open("list_data.json", "r") as a_file:
+        response_json = json.load(a_file)
+        data_frame = obj.list_reformat(response_json)
 
-        if not data_frame.empty:
-            if set(['id', 'created_at', 'subject', 'priority', 'status', 'description']).issubset(data_frame.columns):
-                assert True
+        if set(['id', 'created_at', 'subject', 'priority', 'status', 'description']).issubset(data_frame.columns):
+            assert True
 
-            else:
-                assert False
+        else:
+            assert False
 
+        assert data_frame['id'][10] == 11
+        assert data_frame['priority'][20] == "Unknown"
+        assert data_frame['status'][15] == 'open'
+        assert data_frame['created_at'][17] == '2021-07-28T12:58:12Z'
 
-        response = None
-        obj.list_reformat(response)
-        captured = capsys.readouterr()
-        assert captured.out == "No tickets available to show!\n"
+    response = None
+    obj.list_reformat(response)
+    captured = capsys.readouterr()
+    assert captured.out == "No tickets available to show!\n"
 
 
 def test_single_api_call(capsys):
@@ -69,83 +80,65 @@ def test_single_api_call(capsys):
     :param capsys: Captures stdout and stderr
     """
 
-    #obj1 has valid credentials while obj2 has invalid credentials
     obj1 = ticket_viewer()
     obj2 = ticket_viewer(pwd="abcd")
-    exist = False # Flag to check whether id exists in the database
 
-    #Check response for various ticket ids
-    response = obj1.single_api_call(1)
-    if response:
-        assert response.json()['ticket']['id'] == 1
-        assert response.url == "https://zccstudentshelp.zendesk.com/api/v2/tickets/1.json"
+    #checks if the correct get request is made for different credentials and ids
+    with patch('ticket_viewer.requests.get') as mocked_get:
+        obj1.single_api_call(1)
+        mocked_get.assert_called_with('https://zccstudentshelp.zendesk.com/api/v2/tickets/1.json',
+                                      auth=(os.environ.get('zcc_user') + '/token', os.environ.get('zcc_pwd1')))
 
-        exist = True
+    with patch('ticket_viewer.requests.get') as mocked_get:
+        obj1.single_api_call(10)
+        mocked_get.assert_called_with('https://zccstudentshelp.zendesk.com/api/v2/tickets/10.json',
+                                      auth=(os.environ.get('zcc_user') + '/token', os.environ.get('zcc_pwd1')))
 
-    else:
-        captured = capsys.readouterr()
-        assert captured.out == "Please enter a valid ticket id.\n"
+    with patch('ticket_viewer.requests.get') as mocked_get:
+        obj1.single_api_call(50)
+        mocked_get.assert_called_with('https://zccstudentshelp.zendesk.com/api/v2/tickets/50.json',
+                                      auth=(os.environ.get('zcc_user') + '/token', os.environ.get('zcc_pwd1')))
 
-    response = obj1.single_api_call(10)
-    if response:
-        assert response.json()['ticket']['id'] == 10
-        assert response.url == "https://zccstudentshelp.zendesk.com/api/v2/tickets/10.json"
-
-    else:
-        captured = capsys.readouterr()
-        assert captured.out == "Please enter a valid ticket id.\n"
-
-
-    response = obj1.single_api_call(50)
-    if response:
-        assert response.json()['ticket']['id'] == 50
-        assert response.url == "https://zccstudentshelp.zendesk.com/api/v2/tickets/50.json"
-
-    else:
-        captured = capsys.readouterr()
-        assert captured.out == "Please enter a valid ticket id.\n"
-
-
-    obj1.single_api_call(1087)
-    captured = capsys.readouterr()
-    assert captured.out == "Please enter a valid ticket id.\n"
-
-    if exist:
-        obj2.single_api_call(1)
-        captured = capsys.readouterr()
-        assert "HTTP error occurred: 401 Client Error:" in captured.out
+    with patch('ticket_viewer.requests.get') as mocked_get:
+        obj2.single_api_call(50)
+        mocked_get.assert_called_with('https://zccstudentshelp.zendesk.com/api/v2/tickets/50.json',
+                                      auth=(os.environ.get('zcc_user') + '/token', "abcd"))
 
 
 def test_display_single(capsys):
     """
     Checks if the correct output is displayed for given data
+    We don't have to when response is null as the function isn't
+    called in that scenario
     :param capsys: Captures stdout and stderr
     """
 
     obj1 = ticket_viewer()
 
-    response = obj1.single_api_call(1)
-    if response:
-        obj1.display_single(response.json())
+    #loads the data for various ticket ids and checks if the correct output is displayed
+    with open("data1.json", "r") as a_file:
+        response_json = json.load(a_file)
+        obj1.display_single(response_json)
         captured = capsys.readouterr()
+
         assert "id: 1" in captured.out
+        assert "priority: Unknown" in captured.out
+        assert "subject: Unknown" in captured.out
 
-    response = obj1.single_api_call(10)
-    if response:
-        obj1.display_single(response.json())
+    with open("data10.json", "r") as a_file:
+        response_json = json.load(a_file)
+        obj1.display_single(response_json)
         captured = capsys.readouterr()
+
         assert "id: 10" in captured.out
+        assert "priority: Unknown" in captured.out
+        assert "subject: magna reprehenderit nisi est cillum" in captured.out
 
-    response = obj1.single_api_call(50)
-    if response:
-        obj1.display_single(response.json())
+    with open("data50.json", "r") as a_file:
+        response_json = json.load(a_file)
+        obj1.display_single(response_json)
         captured = capsys.readouterr()
+
         assert "id: 50" in captured.out
-
-
-
-
-
-
-
-
+        assert "priority: Unknown" in captured.out
+        assert "subject: officia magna velit nostrud ullamco" in captured.out
